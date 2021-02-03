e947dcf69c7ed5aa915e2f5bf8933658baa174b9
Artem Machnev <art.machnev@gmail.com>
2021-02-03 10:12:57 -0500

    AM - Added skeleton and test cases for DecodeWay

---
+++ CodingChallenge/DecodeWays/DecodeWays.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DecodeWays
+{
+    public class Program
+    {
+        public int NumDecodings(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
