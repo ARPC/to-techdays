028c066a0b4a8a7627b50a588c32e360871b5043
Artem Machnev <art.machnev@gmail.com>
2021-09-02 19:54:02 -0700

    AM - Added solution for FindTownJudge

--- CodingChallenge/FindTownJudge/FindTownJudge.cs
+++ CodingChallenge/FindTownJudge/FindTownJudge.cs
@@ -6,7 +6,23 @@ namespace FindTownJudge
     {
         public int FindTownJudge(int n, int[,] trust)
         {
-            return 0;
+            int[] outdegree = new int[n];   //  trusts that many people
+            int[] indegree = new int[n];    //  trusted by that many people
+            int numberOfTrusts = trust.GetLength(0);
+
+            for (int i = 0; i < numberOfTrusts; i++)
+            {
+                outdegree[trust[i, 0] - 1] ++;
+                indegree[trust[i, 1] - 1] ++;
+            }
+            
+            for (int i = 0; i < n; i++)
+            {
+                if (outdegree[i] == 0 && indegree[i] == n - 1)
+                    return i + 1;
+            }
+            
+            return -1;
         }
     }
 }
