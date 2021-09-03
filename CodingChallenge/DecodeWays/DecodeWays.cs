ec35c9545cd604633d2ee2bf7220711f63c19995
Artem Machnev <art.machnev@gmail.com>
2021-09-02 19:16:50 -0700

    AM - Added solution for DecodeWays

--- CodingChallenge/DecodeWays/DecodeWays.cs
+++ CodingChallenge/DecodeWays/DecodeWays.cs
@@ -6,7 +6,41 @@ namespace DecodeWays
     {
         public int NumDecodings(string s)
         {
-            throw new NotImplementedException();
-        }
+            int[] dp = new int[s.Length];
+
+            for (int i=s.Length-1; i>= 0; i--)
+            {
+                int digit = s[i] - '0';
+                int num = 0;
+                
+                if (digit > 0)
+                {
+                    //  evaluate as a single-digit number
+                    if (i == s.Length - 1) 
+                        num += 1;    //  end of the string
+                    else
+                        num += dp[i + 1];
+                    
+                    //  evaluate as a two-digit number
+                    if (i <= s.Length - 2)
+                    {
+                        int nextDigit = s[i + 1] - '0';
+                        int twoDigitNum = digit * 10 + nextDigit;
+                        
+                        if (twoDigitNum <= 26)
+                        {
+                            if (i == s.Length - 2)
+                                num += 1; // end of the string
+                            else
+                                num += dp[i + 2];
+                        }
+                    }                    
+                }
+
+                dp[i] = num;
+            }
+
+            return dp[0];
+        }    
     }
 }
--- CodingChallenge/DecodeWays/readme.md
+++ CodingChallenge/DecodeWays/readme.md
@@ -8,38 +8,3 @@ A message containing letters from A-Z can be encoded into numbers using the foll
 To decode an encoded message, all the digits must be mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "111" can have each of its "1"s be mapped into 'A's to make "AAA", or it could be mapped to "11" and "1" ('K' and 'A' respectively) to make "KA". Note that "06" cannot be mapped into 'F' since "6" is different from "06".
 
 Given a non-empty string num containing only digits, return the number of ways to decode it.
-
-      1234
-|              |
-234            34
-|       |     |   |
-34      4     4  null
-|  |    |     |
-4 null null  null
-
-
-1234
-    1 -> 234
-        2 -> 34
-            3 -> 4
-                4 -> null
-            34 -> null
-        23 -> 4
-            4 -> null
-    12 -> 34
-        3 -> 4
-            4 -> null
-        34 -> null
-
- 1  2  3  4
-[2+1, 1+1, 1+0, 1]
-nums = 3
-= 1,2,3,4 12,3,4 1,23,4 
-
- 2, 2, 2, 2
-[3+2, 2+1, 1+1, 1]
-= 5
-- 2,2,2,2 | 22,2,2 | 2,22,2 | 2,2,22 | 22,22
-
-
-
