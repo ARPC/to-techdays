e6e21eb589037746a0c6b36478bdb885cf65a1ae
Artem Machnev <art.machnev@gmail.com>
2021-09-03 12:59:46 -0700

    AM - Added solution

--- CodingChallenge/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.cs
+++ CodingChallenge/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.cs
@@ -6,7 +6,18 @@ namespace BestTimeToBuyAndSellStock
     {
         public int MaxProfit(int[] prices)
         {
-            return 0;
+            //return 0;
+
+            int profit = 0;
+        
+            for (int i = 1; i < prices.Length; i++)
+            {
+                int diff = prices[i] - prices[i - 1];
+                if (diff > 0)
+                    profit += diff;
+            }
+            
+            return profit;
         }
     }
 }
--- CodingChallenge/LongestCommonSubsequence/LongestCommonSubsequence.cs
+++ CodingChallenge/LongestCommonSubsequence/LongestCommonSubsequence.cs
@@ -6,7 +6,21 @@ namespace LongestCommonSubsequence
     {
         public int LongestCommonSubsequence(string text1, string text2)
         {
-            return 0;
+//            return 0;
+
+            int[,] memo = new int[text1.Length+1, text2.Length+1];
+            
+            for(int i=text1.Length-1; i>=0; i--)
+            {
+                for(int j=text2.Length-1; j>=0; j--)
+                {
+                    if (text1[i] != text2[j])
+                        memo[i, j] = Math.Max(memo[i+1, j], memo[i, j+1]);
+                    else
+                        memo[i, j] = 1 + memo[i+1, j+1];
+                }
+            }
+            return memo[0,0];
         }    
     }
 }
